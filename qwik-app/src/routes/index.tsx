5550cf76fdd9410da614eee44285708c59d96f4e
StanPreschlack <91580726+StanPreschlack@users.noreply.github.com>
2022-10-30 18:35:50 -0400

    qwik commit

---
+++ qwik-app/src/routes/index.tsx
@@ -0,0 +1,14 @@
+import { component$ } from '@builder.io/qwik';
+import type { DocumentHead } from '@builder.io/qwik-city';
+
+export default component$(() => {
+  return (
+    <div>
+        
+    </div>
+  );
+});
+
+export const head: DocumentHead = {
+  title: 'Welcome to Qwik',
+};
