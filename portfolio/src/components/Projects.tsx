c432768349dd168d82c22510b4f5ccd04e4f7fff
StanPreschlack <91580726+StanPreschlack@users.noreply.github.com>
2022-11-06 11:44:57 -0500

    added project page message

--- portfolio/src/components/Projects.tsx
+++ portfolio/src/components/Projects.tsx
@@ -2,8 +2,8 @@ import { component$ } from '@builder.io/qwik'
 
 export default component$(() => {
     return (
-        <div>
-            
+        <div id="projects">
+            <p>I will be updating this page shortly! In the meantime you can find a lot of my code on my github <a rel="" href="https://github.com/StanPreschlack" target="_blank">here</a> </p>
         </div>
     )
 })
