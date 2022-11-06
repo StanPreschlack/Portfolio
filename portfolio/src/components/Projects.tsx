6fbda3015d57308cc216ad26aec4d1a7e02e3de7
StanPreschlack <91580726+StanPreschlack@users.noreply.github.com>
2022-11-06 11:55:43 -0500

    final deploy commit

--- portfolio/src/components/Projects.tsx
+++ portfolio/src/components/Projects.tsx
@@ -3,7 +3,7 @@ import { component$ } from '@builder.io/qwik'
 export default component$(() => {
     return (
         <div id="projects">
-            <p>I will be updating this page shortly! In the meantime you can find a lot of my code on my github <a rel="" href="https://github.com/StanPreschlack" target="_blank">here</a> </p>
+            <p>I will be updating this page shortly! In the meantime you can find a lot of my code on my github <a rel="" href="https://github.com/StanPreschlack" target="_blank">HERE</a> </p>
         </div>
     )
 })
