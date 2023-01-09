863043cda3f5050cf64e590599a0cbe8ff303f1e
StanPreschlack <91580726+StanPreschlack@users.noreply.github.com>
2023-01-08 19:56:12 -0500

    added new project

--- portfolio/src/components/Projects.tsx
+++ portfolio/src/components/Projects.tsx
@@ -6,8 +6,8 @@ export default component$(() => {
         <>
             <div id="projects">
                 <Project src="images/docs.png" name="Document Editor" ref="https://docu-share-2daee.web.app/" techOne='images/vue.png' techTwo='images/firebase.png' tecRefOne='https://en.wikipedia.org/wiki/vue.js' tecRefTwo='https://en.wikipedia.org/wiki/firebase'/>
+                <Project src="images/roomates.png" name="Community Roomate Finder" ref="https://nyu-find-roommates.web.app/" techOne='images/vue.png' techTwo='images/firebase.png' tecRefOne='https://en.wikipedia.org/wiki/vue.js' tecRefTwo='https://en.wikipedia.org/wiki/firebase'/>
             </div>
-            <p id="msg">I will be updating this page shortly! In the meantime you can find a lot of my code on my github <a rel="" href="https://github.com/StanPreschlack" target="_blank">HERE</a> </p>
         </>
     )
 })
