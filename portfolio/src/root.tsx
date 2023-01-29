df1b5fd0c89ff37a4c24410405ff542d92c75322
StanPreschlack <91580726+StanPreschlack@users.noreply.github.com>
2023-01-29 00:10:28 -0500

    particles testing

--- portfolio/src/root.tsx
+++ portfolio/src/root.tsx
@@ -39,7 +39,7 @@ export default component$(() => {
             document.documentElement.style.setProperty('--highlight-color', 'rgb(237, 192, 67)')
           }
         }}>{store.theme? "Light" : "Dark"} theme</button>
-        <script type="module" src="/Users/spreschlack/Desktop/software projects/portfolio/portfolio/src/particles/particles.js"></script>
+        <script type="module" src="https://github.com/StanPreschlack/portfolio/blob/c73b080aa733138df7c778c67a9f4b38ae7505c3/portfolio/src/particles/particles.js"></script>
       </body>
     </QwikCity>
   )
